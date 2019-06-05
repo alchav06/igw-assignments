f9500f6e6332a4d8c3853e2d598735ede6494a38
alchav06 <49372118+alchav06@users.noreply.github.com>
2019-06-05 14:00:24 +0200

    Add files via upload

--- execute.py
+++ execute.py
@@ -2,10 +2,9 @@
 
 import requests, os
 
-payload = open(os.path.dirname(os.path.abspath(__file__)) +"\\chaining_example.xml").read()
+payload = open(os.path.dirname(os.path.abspath(__file__)) +"\\intersection_wps_template.xml").read()
 
-
-wpsServerUrl = "http://130.89.221.193:85/geoserver/ows?"
+wpsServerUrl = "https://gisedu.itc.utwente.nl/student/s6037054/igw_wps_assignment/wps.py?"
 
 response = requests.post(wpsServerUrl, data=payload)
 print("Content-type: application/json")
---
+++ intersection_wps_function.py
@@ -0,0 +1,35 @@
+# ---------------------
+# Coordinate Transformation function using WPS specification
+# ---------------------
+from osgeo import ogr
+
+def title():
+    return "Intersection of two features" # title of the function
+
+def abstract():
+    return "A function that intersects two features (line, polygon or point type) and returns the intersected feture in JSON format." # short description of the function
+
+def inputs():
+    return [
+        ['geom1', 'Input feature','The first geometry feature input.','application/wkt', True],
+		['geom2', 'Input feature','The second geometry feature input.','application/wkt', True]	
+    ]
+
+def outputs():
+    return [['result', 'intersected feature','The feature of the intersection of the two inputs','application/json']]
+
+def execute(parameters):
+    featureA = parameters.get('geom1')
+    featureB = parameters.get('geom2')
+
+    if (featureA is not None) and (featureB is not None):
+        featureA = featureA['value']
+        featureB = featureB['value']
+
+
+    featureA = ogr.CreateGeometryFromWkt(featureA)
+    featureB = ogr.CreateGeometryFromWkt(featureB)
+    intersection = featureA.Intersection(featureB)	
+    print("Content-type: application/json")
+    print()
+    print(intersection.ExportToJson())
